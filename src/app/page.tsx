08daf24e757660f34c565bfa07fe6b8dc2f8ff29
Rohit Singh <rohit99.ind@gmail.com>
2024-02-22 00:00:33 +0530

    feat: playground init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,285 @@
+"use client";
+
 import Image from "next/image";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism-dark.css";
+import { use, useEffect, useState } from "react";
+import * as t from "@babel/types";
+
+import { default as traverse } from "@babel/traverse";
+import { parse } from "@babel/parser";
+import { default as generate } from "@babel/generator";
+
+function transformCode(code: string, CONFIG: any) {
+  let importName = "react-native-ustyle";
+  let importedComponents = [];
+  let styleId = 0;
+  let Styles = [];
+  let styleExpression = [];
+  function resolver(name) {
+    if (name in CONFIG) {
+      return CONFIG[name];
+    }
+
+    return name;
+  }
+  function checkIfStylesheetImportedAndImport(programPath) {
+    let importDeclaration = programPath.node.body.find(
+      (node) =>
+        node.type === "ImportDeclaration" &&
+        node.source.value === "react-native"
+    );
+    if (importDeclaration) {
+      // delete the old importspecifier
+      // importDeclaration.specifiers = importDeclaration.specifiers.filter(
+      //   (specifier) => specifier.imported.name !== "StyleSheet"
+      // );
+      // programPath.node.body.unshift(
+      //   t.importDeclaration(
+      //     [
+      //       t.importSpecifier(
+      //         t.identifier("StyleSheet"),
+      //         t.identifier("StyleSheet")
+      //       ),
+      //     ],
+      //     t.stringLiteral("react-native")
+      //   )
+      // );
+    }
+  }
+  function attributesToObject(attributes) {
+    if (!Array.isArray(attributes)) {
+      throw new TypeError("Expected attributes to be an array");
+    }
+
+    const obj = {};
+    attributes.forEach((attribute) => {
+      const key = resolver(attribute.name.name);
+      let value;
+      if (attribute.value.type === "JSXExpressionContainer") {
+        if (attribute.value.expression.type === "ObjectExpression") {
+          value = {};
+          attribute.value.expression.properties.forEach((prop) => {
+            const propName =
+              prop.key.type === "StringLiteral"
+                ? prop.key.value
+                : prop.key.name;
+            if (prop.value.value !== undefined) {
+              value[propName] = prop.value.value;
+            }
+          });
+        } else {
+          value = attribute.value.expression.value;
+        }
+      } else if (attribute.value.type === "JSXElement") {
+        value = attributesToObject(attribute.value.openingElement.attributes);
+      } else {
+        value = attribute.value.value;
+      }
+
+      if (value !== undefined) {
+        obj[key] = value;
+      }
+    });
+    return obj;
+  }
+  function addRnuStyleIdInStyleArrayOfCOmponent(jsxAttrArray, styleId) {
+    // find the style attribute
+    let styleAttr = jsxAttrArray.find((attr) => attr.name.name === "style");
+    // insert the styleId in the style array
+    // style can be an array or a single object
+    if (styleAttr) {
+      // if the style attribute is a single object then convert it to an array
+      if (styleAttr.value.expression.type !== "ArrayExpression") {
+        styleAttr.value.expression = t.arrayExpression([
+          styleAttr.value.expression,
+        ]);
+      }
+      let styleArray = styleAttr.value.expression.elements;
+      styleArray.push(t.identifier("rnuStyles.styles" + styleId));
+    } else {
+      // create a new style attribute
+      jsxAttrArray.push(
+        t.jsxAttribute(
+          t.jsxIdentifier("style"),
+          t.jsxExpressionContainer(
+            t.arrayExpression([t.identifier("rnuStyles.styles" + styleId)])
+          )
+        )
+      );
+    }
+  }
+  let ast;
+  try {
+    ast = parse(code, {
+      sourceType: "module",
+      plugins: ["jsx", "typescript"],
+    });
+  } catch (e) {
+    console.error("AST PARSING ERROR", e);
+  }
+  try {
+    traverse(ast, {
+      ImportDeclaration(path) {
+        if (path.node.source.value === importName) {
+          path.traverse({
+            ImportSpecifier(path) {
+              importedComponents.push(path.node.local.name);
+            },
+          });
+          path.node.source.value = "react-native";
+        }
+      },
+      JSXOpeningElement(path) {
+        if (importedComponents.includes(path.node.name.name)) {
+          // Create a variable declaration for the object
+          addRnuStyleIdInStyleArrayOfCOmponent(path.node.attributes, styleId);
+          styleExpression.push(
+            t.objectProperty(
+              t.identifier("styles" + styleId++),
+              t.valueToNode(attributesToObject(path.node.attributes))
+            )
+          );
+          // check if rnuStyles is already declared
+          let declaration = ast.program.body.find(
+            (node) =>
+              node.type === "VariableDeclaration" &&
+              node.declarations[0].id.name === "rnuStyles"
+          );
+          if (declaration) {
+            ast.program.body = ast.program.body.filter(
+              (node) =>
+                node.type !== "VariableDeclaration" ||
+                node.declarations[0].id.name !== "rnuStyles"
+            );
+          } else {
+            declaration = t.variableDeclaration("const", [
+              t.variableDeclarator(
+                t.identifier("rnuStyles"),
+                // Not using StyleSheet.create since it is not working in the latest metro version
+                // t.callExpression(t.identifier("StyleSheet.create"), [
+                t.objectExpression(styleExpression)
+                // ])
+              ),
+            ]);
+          }
+          Styles.push(declaration);
+          ast.program.body.push(declaration);
+        }
+      },
+      Program(path) {
+        checkIfStylesheetImportedAndImport(path);
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
+  return generate(ast).code;
+}
 
 export default function Home() {
+  const [inputCode, setInputCode] = useState(
+    `import React from 'react';
+import { Text, View } from 'react-native-ustyle';
+
+export default function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <View bg="yellow" p={20} mx={20}>
+      <Text c="blue">Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}   
+`
+  );
+  const [config, setConfig] = useState({
+    p: "padding",
+    m: "margin",
+    t: "top",
+    b: "bottom",
+    l: "left",
+    r: "right",
+    h: "height",
+    w: "width",
+    bg: "backgroundColor",
+    c: "color",
+    mx: "marginHorizontal",
+    bc: "borderColor",
+    bw: "borderWidth",
+    mr: "marginRight",
+  });
+  const [activeFile, setActiveFile] = useState("app");
+  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
+
+  return (
+    <main className="flex h-screen flex-row items-center justify-between p-24 gap-2">
+      <div className="flex flex-col h-full flex-1 border-white rounded-md border overflow-hidden">
+        <div className="flex flex-row border-b-2">
+          <div
+            onClick={() => setActiveFile("app")}
+            className={
+              activeFile === "app"
+                ? "cursor-pointer px-4 py-2 bg-gray-700"
+                : "cursor-pointer px-4 py-2 bg-gray-900"
+            }
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            App.tsx
+          </div>
+          <div
+            onClick={() => setActiveFile("config")}
+            className={
+              activeFile === "config"
+                ? "cursor-pointer px-4 py-2 bg-gray-700"
+                : "cursor-pointer px-4 py-2 bg-gray-900"
+            }
+          >
+            rnu.config.ts
+          </div>
         </div>
+        {activeFile === "app" ? (
+          <Editor
+            value={inputCode}
+            onValueChange={(code) => {
+              setInputCode(code);
+              setCode(code);
+            }}
+            highlight={(code) => highlight(code, languages.js)}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+              width: "100%",
+              height: "100%",
+              overflow: "scroll",
+            }}
+          />
+        ) : (
+          <div>
+            {Object.keys(config).map((key, ind) => {
+              return (
+                <div>
+                  {key}:{config[key]}
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
+      <div className="flex h-full flex-1 border-white rounded-md border overflow-hidden">
+        <Editor
+          value={transformCode(inputCode, config)}
+          onValueChange={() => {}}
+          highlight={(code) => highlight(code, languages.js)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+            fontSize: 12,
+            width: "100%",
+          }}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
